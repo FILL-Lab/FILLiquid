851c27a5ce3e09cda3a4450cdc5ee7739017e005
jinqiao0423 <jinqiao0423@gmail.com>
2023-07-20 16:51:39 +0800

    logic fix

--- contracts/Governance.sol
+++ contracts/Governance.sol
@@ -40,36 +40,26 @@ contract Governance is Context {
         uint[4] amounts;
     }
 
-    struct Proposal {
-        proposolCategory category;
-        uint deadline;
-        uint modificationline;
-        uint deposited;
-        uint discussionIndex;
-        bool executed;
-        string text;
-        address proposer;
-        uint[] values;
-        VotingStatus status;
-    }
-
     struct ProposalInfo {
         proposolCategory category;
         uint deadline;
-        uint modificationline;
         uint deposited;
         uint discussionIndex;
         bool executed;
         string text;
         address proposer;
         uint[] values;
+    }
+
+    struct Proposal {
+        ProposalInfo info;
+        VotingStatus status;
     }
 
     event Proposed (
         uint proposalId,
         proposolCategory category,
         uint deadline,
-        uint modificationline,
         uint deposited,
         uint discussionIndex,
         string text,
@@ -94,13 +84,6 @@ contract Governance is Context {
         address voter
     );
 
-    event Withdrawn (
-        uint proposalId,
-        voteCategory category,
-        uint amount,
-        address voter
-    );
-
     event Executed (
         uint proposalId,
         voteResult result,
@@ -117,10 +100,10 @@ contract Governance is Context {
     uint private _maxNo;
     uint private _maxNoWithVeto;
     uint private _quorum;
+    uint private _liquidate;
     uint private _depositThreshold;
     uint private _voteThreshold;
     uint private _votingPeriod;
-    uint private _modifiablePeriod;
     uint private _executionPeriod;
     uint private _maxActiveProposal;
     address private _owner;
@@ -134,12 +117,12 @@ contract Governance is Context {
     uint constant DEFAULT_MAX_NO = 333333;
     uint constant DEFAULT_MAX_NO_WITH_VETO = 333333;
     uint constant DEFAULT_QUORUM = 400000;
-    uint constant DEFAULT_DEPOSIT_THRESHOLD = 1e22; // TODO: discuss about the value
-    uint constant DEFAULT_VOTE_THRESHOLD = 1e20; // TODO: discuss about the value
-    uint constant DEFAULT_VOTING_PERIOD = 40320; // 14 days, todo: discuss about the value
-    uint constant DEFAULT_MODIFIABLE_PERIOD = 25920; // 9 days, todo: discuss about the value
-    uint constant DEFAULT_EXECUTION_PERIOD = 40320; // 14 days, todo: discuss about the value
-    uint constant DEFAULT_MAX_ACTIVE_PROPOSAL = 1000; // todo: discuss about the value
+    uint constant DEFAULT_LIQUIDATE = 200000;
+    uint constant DEFAULT_DEPOSIT_THRESHOLD = 5e20;
+    uint constant DEFAULT_VOTE_THRESHOLD = 1e19;
+    uint constant DEFAULT_VOTING_PERIOD = 40320; // 14 days
+    uint constant DEFAULT_EXECUTION_PERIOD = 20160; // 7 days
+    uint constant DEFAULT_MAX_ACTIVE_PROPOSAL = 1000;
 
     constructor() {
         _owner = _msgSender();
@@ -149,10 +132,10 @@ contract Governance is Context {
         _maxNo = DEFAULT_MAX_NO;
         _maxNoWithVeto = DEFAULT_MAX_NO_WITH_VETO;
         _quorum = DEFAULT_QUORUM;
+        _liquidate = DEFAULT_LIQUIDATE;
         _depositThreshold = DEFAULT_DEPOSIT_THRESHOLD;
         _voteThreshold = DEFAULT_VOTE_THRESHOLD;
         _votingPeriod = DEFAULT_VOTING_PERIOD;
-        _modifiablePeriod = DEFAULT_MODIFIABLE_PERIOD;
         _executionPeriod = DEFAULT_EXECUTION_PERIOD;
         _maxActiveProposal = DEFAULT_MAX_ACTIVE_PROPOSAL;
     }
@@ -182,33 +165,42 @@ contract Governance is Context {
     }
 
     function propose(proposolCategory category, uint discussionIndex, string memory text, uint[] memory values) external {
-        require (_proposals.length - getFirstVotingProposal() < _maxActiveProposal, "Max active proposals reached");
+        require (_proposals.length - renewFirstVotingProposal() < _maxActiveProposal, "Max active proposals reached");
         _checkParameter(category, values);
         address sender = _msgSender();
         _tokenFILGovernance.claim(sender, _depositThreshold);
         _proposals.push();
-        Proposal storage p = _proposals[_proposals.length - 1];
-        p.category = category;
-        p.deadline = block.number + _votingPeriod;
-        p.modificationline = block.number + _modifiablePeriod;
-        p.deposited = _depositThreshold;
-        p.discussionIndex = discussionIndex;
-        p.text = text;
-        p.proposer = sender;
-        p.values = values;
+        ProposalInfo storage info = _proposals[_proposals.length - 1].info;
+        info.category = category;
+        info.deadline = block.number + _votingPeriod;
+        info.deposited = _depositThreshold;
+        info.discussionIndex = discussionIndex;
+        info.text = text;
+        info.proposer = sender;
+        info.values = values;
 
-        emit Proposed(_proposals.length - 1, p.category, p.deadline, p.modificationline, p.deposited, p.discussionIndex, p.text, p.proposer, p.values);
+        emit Proposed(
+            _proposals.length - 1,
+            info.category,
+            info.deadline,
+            info.deposited,
+            info.discussionIndex,
+            info.text,
+            info.proposer,
+            info.values
+        );
     }
 
     function vote(uint proposalId, voteCategory category, uint amount) validProposalId(proposalId) external {
         Proposal storage p = _proposals[proposalId];
-        require(p.deadline >= block.number, "Proposal finished");
-        address voter = _msgSender();
-        require(amount >= _voteThreshold, "Voting amount too low");
+        ProposalInfo storage info = p.info;
         VotingStatus storage status = p.status;
+        address voter = _msgSender();
         Voting storage voting = status.voterVotings[voter];
-        if (voting.amountTotal == 0) status.voters.push(voter);
+        require(amount >= _voteThreshold, "Voting amount too low");
+        require(info.deadline >= block.number, "Proposal finished");
         require(voting.amountTotal + amount <= _bondings[voter], "Invalid amount");
+        if (voting.amountTotal == 0) status.voters.push(voter);
         status.amountTotal += amount;
         status.amounts[uint(category)] += amount;
         voting.amountTotal += amount;
@@ -217,59 +209,41 @@ contract Governance is Context {
         emit Voted(proposalId, category, amount, voter);
     }
 
-    function withdraw(uint proposalId, voteCategory category, uint amount) validProposalId(proposalId) external {
-        Proposal storage p = _proposals[proposalId];
-        require(p.modificationline >= block.number, "Proposal finished");
-        address voter = _msgSender();
-        VotingStatus storage status = p.status;
-        Voting storage voting = status.voterVotings[voter];
-        uint maxAmount = voting.amounts[uint(category)];
-        require(amount <= maxAmount, "Invalid amount");
-        if (amount == 0) amount = maxAmount;
-        status.amountTotal -= amount;
-        status.amounts[uint(category)] -= amount;
-        voting.amountTotal -= amount;
-        voting.amounts[uint(category)] -= amount;
-        if (voting.amountTotal == 0) {
-            address[] storage voters = status.voters;
-            for (uint i = 0; i < voters.length; i++) {
-                if (voters[i] == voter) {
-                    if (i != voters.length - 1) {
-                        voters[i] = voters[voters.length - 1];
-                    }
-                    voters.pop();
-                    break;
-                }
-            }
-        }
-
-        emit Withdrawn(proposalId, category, amount, voter);
-    }
-
     function execute(uint proposalId) validProposalId(proposalId) external {
         Proposal storage p = _proposals[proposalId];
-        require(p.deadline < block.number, "Proposal voting in progress");
-        require(p.deadline + _executionPeriod >= block.number, "Proposal expired");
-        require(!p.executed, "Already executed");
-        _nextProposalId = proposalId + 1;
-        p.executed = true;
+        ProposalInfo storage info = p.info;
         VotingStatus storage status = p.status;
+        address sender = _msgSender();
+        require(info.deadline < block.number, "Proposal voting in progress");
+        require(!info.executed, "Already executed");
+        info.executed = true;
+        if (_nextProposalId <= proposalId) {
+            _nextProposalId = proposalId + 1;
+        }
         voteResult result = _voteResult(status);
         if (result == voteResult.failWithVeto) {
-            _tokenFILGovernance.burn(address(this), p.deposited);
+            (uint liquidate, uint remain) = _liquidateResult(info.deposited);
+            _tokenFILGovernance.transfer(sender, liquidate);
+            _tokenFILGovernance.burn(address(this), remain);
         } else {
-            if (result == voteResult.success) {
-                if (p.category == proposolCategory.filLiquid) _filLiquid.setBorrowPayBackFactors(p.values);
-                else if (p.category == proposolCategory.filStake) _filStake.setGovernanceFactors(p.values);
+            if (info.deadline + _executionPeriod >= block.number) {
+                if (result == voteResult.success) {
+                    if (info.category == proposolCategory.filLiquid) _filLiquid.setBorrowPayBackFactors(info.values);
+                    else if (info.category == proposolCategory.filStake) _filStake.setGovernanceFactors(info.values);
+                }
+                _tokenFILGovernance.transfer(info.proposer, info.deposited);
+            } else {
+                (uint liquidate, uint remain) = _liquidateResult(info.deposited);
+                _tokenFILGovernance.transfer(sender, liquidate);
+                _tokenFILGovernance.transfer(info.proposer, remain);
             }
-            _tokenFILGovernance.transfer(p.proposer, p.deposited);
         }
 
         emit Executed(proposalId, result, _msgSender());
     }
 
-    function votingProposalSum(address bonder) public view returns (uint count, uint maxVote) {
-        for (uint i = getFirstVotingProposal(); i < _proposals.length; i++) {
+    function votingProposalSum(address bonder) public returns (uint count, uint maxVote) {
+        for (uint i = renewFirstVotingProposal(); i < _proposals.length; i++) {
             uint amount = _proposals[i].status.voterVotings[bonder].amountTotal;
             if (amount > 0) {
                 if (amount > maxVote) maxVote = amount;
@@ -282,33 +256,23 @@ contract Governance is Context {
         return _bondings[bonder];
     }
 
-    function getFirstVotingProposal() public view returns (uint i) {
+    function renewFirstVotingProposal() public returns (uint i) {
         for (i = _nextProposalId; i < _proposals.length; i++) {
-            if (_proposals[i].deadline >= block.number) break;
+            if (_proposals[i].info.deadline >= block.number) break;
         }
+        _nextProposalId = i;
     }
 
-    function votedForProposal(address bonder, uint proposalId) validProposalId(proposalId) external view returns (Voting memory) {
-        return _proposals[proposalId].status.voterVotings[bonder];
+    function votedForProposal(address voter, uint proposalId) validProposalId(proposalId) external view returns (Voting memory) {
+        return _proposals[proposalId].status.voterVotings[voter];
     }
 
     function getStatus() external view returns (uint, uint, uint) {
         return (_bonders, _totalBondedAmount, _nextProposalId);
     }
 
-    function getProposalInfo(uint proposalId) validProposalId(proposalId) external view returns (ProposalInfo memory result) {
-        Proposal storage proposal = _proposals[proposalId];
-        result = ProposalInfo(
-            proposal.category,
-            proposal.deadline,
-            proposal.modificationline,
-            proposal.deposited,
-            proposal.discussionIndex,
-            proposal.executed,
-            proposal.text,
-            proposal.proposer,
-            proposal.values
-        );
+    function getProposalInfo(uint proposalId) validProposalId(proposalId) external view returns (ProposalInfo memory) {
+        return _proposals[proposalId].info;
     }
 
     function proposalCount() external view returns (uint) {
@@ -332,7 +296,7 @@ contract Governance is Context {
     }
 
     function getFactors() external view returns (uint, uint, uint, uint, uint, uint, uint, uint, uint, uint, uint) {
-        return (_rateBase, _minYes, _maxNo, _maxNoWithVeto, _quorum, _depositThreshold, _voteThreshold, _votingPeriod, _modifiablePeriod, _executionPeriod, _maxActiveProposal);
+        return (_rateBase, _minYes, _maxNo, _maxNoWithVeto, _quorum, _liquidate, _depositThreshold, _voteThreshold, _votingPeriod, _executionPeriod, _maxActiveProposal);
     }
 
     function setFactors(
@@ -341,10 +305,10 @@ contract Governance is Context {
         uint new_maxNo,
         uint new_maxNoWithVeto,
         uint new_quorum,
+        uint new_liquidate,
         uint new_depositThreshold,
         uint new_voteThreshold,
         uint new_votingPeriod,
-        uint new_modifiablePeriod,
         uint new_executionPeriod,
         uint new_maxActiveProposal
     ) onlyOwner external {
@@ -353,16 +317,16 @@ contract Governance is Context {
             new_maxNo <= new_rateBase &&
             new_maxNoWithVeto <= new_rateBase &&
             new_quorum <= new_rateBase &&
-            new_modifiablePeriod <= new_votingPeriod, "Invalid factor");
+            new_liquidate <= new_rateBase, "Invalid factor");
         _rateBase = new_rateBase;
         _minYes = new_minYes;
         _maxNo = new_maxNo;
         _maxNoWithVeto = new_maxNoWithVeto;
         _quorum = new_quorum;
+        _liquidate = new_liquidate;
         _depositThreshold = new_depositThreshold;
         _voteThreshold = new_voteThreshold;
         _votingPeriod = new_votingPeriod;
-        _modifiablePeriod = new_modifiablePeriod;
         _executionPeriod = new_executionPeriod;
         _maxActiveProposal = new_maxActiveProposal;
     }
@@ -403,8 +367,7 @@ contract Governance is Context {
     function _checkParameter(proposolCategory category, uint[] memory params) private view {
         if (category == proposolCategory.filLiquid) {
             _filLiquid.checkBorrowPayBackFactors(params);
-        }
-        else if (category == proposolCategory.filStake) {
+        } else if (category == proposolCategory.filStake) {
             _filStake.checkGovernanceFactors(params);
         } else {
             require(params.length == 0, "Invalid input length");
@@ -425,5 +388,12 @@ contract Governance is Context {
         } else {
             result = voteResult.pending;
         }
+    }
+
+    function _liquidateResult(uint amount) private view returns (uint liquidate, uint remain) {
+        liquidate = amount * _liquidate;
+        if (liquidate % _rateBase == 0) liquidate /= _rateBase;
+        else liquidate = liquidate / _rateBase + 1;
+        remain = amount - liquidate;
     }
 }

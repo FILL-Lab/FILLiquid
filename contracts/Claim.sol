b72d0852e97c0c7572b1174e1603d7fa632e4630
dylenfu <dylenfu@126.com>
2024-07-29 15:59:03 +0800

    use ERC20

--- contracts/Claim.sol
+++ contracts/Claim.sol
@@ -2,14 +2,12 @@
 pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/utils/Context.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Claim is Context, ReentrancyGuard {
     
-    using SafeERC20 for IERC20;
-
     // the actions that can be claimed
     enum Action {
         Unknown,
@@ -24,7 +22,7 @@ contract Claim is Context, ReentrancyGuard {
     }
 
     address public immutable _reserve;     // the reserve address, the rest token will be sent to this address  
-    IERC20 public immutable _token;
+    ERC20 public immutable _token;
 
     mapping (Action => uint) public _supplies;                              // mapping action to the supply
     mapping (Action => bytes32) public _merkleRoots;                        // mapping action to the merkle root
@@ -77,7 +75,7 @@ contract Claim is Context, ReentrancyGuard {
 
     constructor(address token, address reserve, bytes32[] memory merkleRoots, uint[] memory userNums) {
         _reserve = reserve;
-        _token = IERC20(token);
+        _token = ERC20(token);
 
         // set up the merkle roots and user numbers, the first action is unkown
         require(merkleRoots.length == userNums.length && userNums.length == CLAIMABLE_ACTIONS, "Claim: invalid length");
@@ -113,7 +111,7 @@ contract Claim is Context, ReentrancyGuard {
 
         uint restBalance = _token.balanceOf(address(this));
         if (restBalance > 0) {
-            _token.safeTransfer(_reserve, restBalance);
+            _token.transfer(_reserve, restBalance);
         }
         return restBalance;
     }
@@ -125,7 +123,7 @@ contract Claim is Context, ReentrancyGuard {
         address account = _msgSender();
         CalculateResult memory r = calculateStake(account, requests);
         if (r.sum > 0) {
-            _token.safeTransfer(account, r.sum);
+            _token.transfer(account, r.sum);
         }
         for (uint i = 0; i < r.data.length; i++) {
             CalculateData memory item = r.data[i];
@@ -143,7 +141,7 @@ contract Claim is Context, ReentrancyGuard {
         address account = _msgSender();
         CalculateResult memory r = calculateBorrow(account, requests);
         if (r.sum > 0) {
-            _token.safeTransfer(account, r.sum);
+            _token.transfer(account, r.sum);
         }
         for (uint i = 0; i < r.data.length; i++) {
             CalculateData memory item = r.data[i];

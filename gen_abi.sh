9986c832eb455d03013f04311e3327fb3ed0f30c
dylenfu <dylenfu@126.com>
2024-07-31 09:59:45 +0800

    delete dumplicate contract

--- gen_abi.sh
+++ gen_abi.sh
@@ -5,3 +5,4 @@ abigen --abi=goabi/fil_liquid.abi --pkg=liquid -out=goabi/fil_liquid_abi.go
 abigen --abi=goabi/governance.abi --pkg=governance -out=goabi/governance_abi.go
 abigen --abi=goabi/fit_stake.abi --pkg=fitstake -out=goabi/fit_stake_abi.go
 abigen --abi=goabi/claim.abi --pkg=claim -out=goabi/claim_abi.go
+abigen --abi=goabi/batch_transfer.abi --pkg=batch -out=goabi/batch_transfer.go
